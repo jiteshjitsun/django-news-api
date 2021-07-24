4dfe5e818a6df47d6df3573f6a03548accf33e6b
jitesh <jiteshjitsun@gmail.com>
2021-07-24 22:43:54 +0530

    deploying

--- NewsAppProject/settings.py
+++ NewsAppProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yb)1@d*y-w!*#=c(q*p05e1efv!a*ifw=uw8xftm@t(lrp)@o#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["djangonewsapi.hrokuapp.com"]
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +124,8 @@ MEDIA_URL = '/images/'
 
 import os
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
